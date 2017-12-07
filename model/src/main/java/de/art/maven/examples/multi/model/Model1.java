7bc7ea6e0b58f2763d3dec755bb17e5034c1d771
Alexander Redmann <redmann@xplace.de>
2017-12-07 14:20:45 +0100

    change to logger instead of sout

--- model/src/main/java/de/art/maven/examples/multi/model/Model1.java
+++ model/src/main/java/de/art/maven/examples/multi/model/Model1.java
@@ -1,7 +1,12 @@
 package de.art.maven.examples.multi.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Model1 {
+    private static final Logger LOG = LoggerFactory.getLogger(Model1.class);
+
     public void print() {
-        System.out.println("Model1");
+        LOG.info("Model2");
     }
 }
--- model/src/main/java/de/art/maven/examples/multi/model/Model2.java
+++ model/src/main/java/de/art/maven/examples/multi/model/Model2.java
@@ -1,9 +1,14 @@
 package de.art.maven.examples.multi.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Model2 extends Model1 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Model2.class);
 
     @Override
     public void print() {
-        System.out.println("Model2");
+        LOG.info("Model2");
     }
 }
