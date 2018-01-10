23b9d4c8e28ccd229cac495bbed5737b4356153f
Alexander Redmann <redmann@xplace.de>
2018-01-10 16:34:41 +0100

    add android example

--- test-app/src/main/c++/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <test.h>
-
-int main(int argc, char** argv) 
-{
-    printf("hello world %d\n", function(10));
-}
--- test-library/src/main/c++/dummy.cpp
+++
@@ -1,7 +0,0 @@
-#define CREATE_LIB // export functions
-#include <test.h>
-
-DLL_DECL int function(int a) 
-{
-    return a + 1;
-}
